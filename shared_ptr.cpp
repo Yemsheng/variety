3677b6a3a417d8db91b5dd0f6074743f42ead159
= <=>
2013-12-30 14:48:14 +0800

    智能指针小试

---
+++ shared_ptr.cpp
@@ -0,0 +1,42 @@
+
+
+#include <iostream>
+#include <string>
+#include <boost/smart_ptr.hpp>
+
+class TC {
+public:
+    TC(std::string strParam):str(strParam){
+        std::cout<<"TC constructor"<<std::endl;
+    }
+    ~TC(){
+        std::cout<<"~TC destructor"<<std::endl;
+    }
+    void print_str(int flag=0){
+        std::cout<<flag<<" "<<str<<std::endl;
+    }
+
+private:
+    std::string str;
+
+};
+
+
+boost::shared_ptr<TC> func_test(){
+    boost::shared_ptr<TC> test_pc(new TC("hello world"));
+    test_pc->print_str(1);
+    std::cout<<"use_count = "<<test_pc.use_count()<<std::endl;
+    return test_pc;
+}
+
+int main(){
+    boost::shared_ptr<TC> test_pc2 = func_test();
+    test_pc2->print_str(2);
+    std::cout<<"test_pc2 use_count = "<<test_pc2.use_count()<<std::endl;
+
+    boost::shared_ptr<TC> test_pc3 = test_pc2;
+    test_pc3->print_str(2);
+    std::cout<<"test_pc3 use_count = "<<test_pc3.use_count()<<std::endl;
+
+    return 0;
+}
