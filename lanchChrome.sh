679bce68ad2f5dd63251c54d4327be52ec2c66e7
= <=>
2014-02-04 13:12:52 +0800

    User Agent

---
+++ lanchChrome.sh
@@ -0,0 +1,31 @@
+#! /bin/sh
+
+# 修改User Agent，防范浏览器指纹
+# 此脚本适用于Mac系统的Chrome
+# Linux系统，需要修改Chrome的路径
+# 使用方法: 每次使用此脚本启动Chrome浏览器, sh lanchChrome.sh
+
+userAgentStr=(
+"Mozilla/5.0 (Windows NT 6.1; Win64; x64; rv:25.0) Gecko/20100101 Firefox/25.0"
+"Mozilla/5.0 (Macintosh; Intel Mac OS X 10.6; rv:25.0) Gecko/20100101 Firefox/25.0"
+"Mozilla/5.0 (X11; Ubuntu; Linux x86_64; rv:24.0) Gecko/20100101 Firefox/24.0"
+"Mozilla/5.0 (Windows NT 6.2; rv:22.0) Gecko/20130405 Firefox/23.0"
+"Mozilla/5.0 (Windows NT 6.1; WOW64; rv:21.0) Gecko/20130401 Firefox/21.0"
+"Mozilla/5.0 (X11; Linux i686; rv:21.0) Gecko/20100101 Firefox/21.0"
+"Mozilla/5.0 (Windows NT 6.2; Win64; x64; rv:21.0.0) Gecko/20121011 Firefox/21.0.0"
+"Mozilla/5.0 (Macintosh; Intel Mac OS X 10_8_3) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/27.0.1453.93 Safari/537.36"
+"Mozilla/5.0 (Windows NT 6.1) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/27.0.1453.93 Safari/537.36"
+"Mozilla/5.0 (Windows NT 5.1) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/31.0.1650.16 Safari/537.36"
+"Mozilla/5.0 (Macintosh; Intel Mac OS X 10_9_0) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/32.0.1664.3 Safari/537.36"
+"Mozilla/5.0 (Windows NT 6.2; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/32.0.1667.0 Safari/537.36"
+)
+
+userAgentTotalNum=${#userAgentStr[*]}
+num=$(date +%s)
+index=$((num%userAgentTotalNum))
+userAgentChoose=${userAgentStr[index]}
+echo $userAgentChoose
+
+cd /Applications/Google\ Chrome.app/Contents/MacOS/
+rm nohup.out
+nohup ./Google\ Chrome --user-agent="$userAgentChoose" &
